1ed3d79f8e3d1fe18af549928869541f0fa2696e
Sergii Tkachenko <sergiitk@google.com>
2020-12-30 07:59:44 -0500

    bin/run_channelz.py break up

--- tools/run_tests/xds_k8s_test_driver/bin/run_channelz.py
+++ tools/run_tests/xds_k8s_test_driver/bin/run_channelz.py
@@ -65,111 +65,119 @@ def get_deployment_pod_ips(k8s_ns, deployment_name):
     return [pod.status.pod_ip for pod in pods]
 
 
+def negative_case_mtls(test_client, test_server):
+    """
+    mTLS Error case: server expects client mTLS cert, but client configured
+    only for TLS.
+    """
+    # Client side.
+    client_correct_setup = True
+    channel: _Channel = test_client.wait_for_server_channel_state(
+        state=_ClientChannelState.TRANSIENT_FAILURE)
+    try:
+        subchannel, *subchannels = list(
+            test_client.channelz.list_channel_subchannels(channel))
+    except ValueError:
+        print("(mTLS-error) Client setup fail: subchannel not found. "
+              "Common causes: test client didn't connect to TD; "
+              "test client exhausted retries, and closed all subchannels.")
+        return
+
+    # Client must have exactly one subchannel.
+    logger.debug('Found subchannel, %r', subchannel)
+    if subchannels:
+        client_correct_setup = False
+        print(f'(mTLS-error) Unexpected subchannels {subchannels}')
+    subchannel_state: _ClientChannelState = subchannel.data.state.state
+    if subchannel_state is not _ClientChannelState.TRANSIENT_FAILURE:
+        client_correct_setup = False
+        print('(mTLS-error) Subchannel expected to be in '
+              'TRANSIENT_FAILURE, same as its channel')
+
+    # Client subchannel must have no sockets.
+    sockets = list(test_client.channelz.list_subchannels_sockets(subchannel))
+    if sockets:
+        client_correct_setup = False
+        print(f'(mTLS-error) Unexpected subchannel sockets {sockets}')
+
+    # Test server must have no sockets on the rpc port.
+    server_correct_setup = True
+    server_sockets = list(test_server.get_test_server_sockets())
+    if server_sockets:
+        server_correct_setup = False
+        print('(mTLS-error) Server setup fail:'
+              f' unexpected sockets {sockets}')
+
+    # Results.
+    if client_correct_setup:
+        print('(mTLS-error) Client setup pass: the channel '
+              'to the server has exactly one subchannel '
+              'in TRANSIENT_FAILURE, and no sockets')
+    if server_correct_setup:
+        print('(mTLS-error) Server setup pass: test server has no sockets')
+
+
+def positive_case_all(test_client, test_server):
+    """All positive cases: mTLS, TLS, Plaintext"""
+    test_client.wait_for_active_server_channel()
+    client_sock: _Socket = test_client.get_active_server_channel_socket()
+    server_sock: _Socket = test_server.get_server_socket_matching_client(
+        client_sock)
+
+    server_tls = server_sock.security.tls
+    client_tls = client_sock.security.tls
+
+    print(f'\nServer certs:\n{debug_sock_tls(server_tls)}')
+    print(f'\nClient certs:\n{debug_sock_tls(client_tls)}')
+    print()
+
+    if server_tls.local_certificate:
+        eq = server_tls.local_certificate == client_tls.remote_certificate
+        print(f'(TLS)  Server local matches client remote: {eq}')
+    else:
+        print('(TLS)  Not detected')
+
+    if server_tls.remote_certificate:
+        eq = server_tls.remote_certificate == client_tls.local_certificate
+        print(f'(mTLS) Server remote matches client local: {eq}')
+    else:
+        print('(mTLS) Not detected')
+
+
 def main(argv):
     if len(argv) > 1:
         raise app.UsageError('Too many command-line arguments.')
 
     k8s_api_manager = k8s.KubernetesApiManager(xds_k8s_flags.KUBE_CONTEXT.value)
 
-    # Namespaces
-    namespace = xds_flags.NAMESPACE.value
-    server_namespace = namespace
-    client_namespace = namespace
-
     # Server
-    server_k8s_ns = k8s.KubernetesNamespace(k8s_api_manager, server_namespace)
     server_name = xds_flags.SERVER_NAME.value
-    server_port = xds_flags.SERVER_PORT.value
+    server_namespace = xds_flags.NAMESPACE.value
+    server_k8s_ns = k8s.KubernetesNamespace(k8s_api_manager, server_namespace)
     server_pod_ip = get_deployment_pod_ips(server_k8s_ns, server_name)[0]
     test_server: _XdsTestServer = _XdsTestServer(
         ip=server_pod_ip,
-        rpc_port=server_port,
+        rpc_port=xds_flags.SERVER_PORT.value,
         xds_host=xds_flags.SERVER_XDS_HOST.value,
         xds_port=xds_flags.SERVER_XDS_PORT.value,
         rpc_host=_SERVER_RPC_HOST.value)
 
     # Client
-    client_k8s_ns = k8s.KubernetesNamespace(k8s_api_manager, client_namespace)
     client_name = xds_flags.CLIENT_NAME.value
-    client_port = xds_flags.CLIENT_PORT.value
+    client_namespace = xds_flags.NAMESPACE.value
+    client_k8s_ns = k8s.KubernetesNamespace(k8s_api_manager, client_namespace)
     client_pod_ip = get_deployment_pod_ips(client_k8s_ns, client_name)[0]
-
     test_client: _XdsTestClient = _XdsTestClient(
         ip=client_pod_ip,
         server_target=test_server.xds_uri,
-        rpc_port=client_port,
+        rpc_port=xds_flags.CLIENT_PORT.value,
         rpc_host=_CLIENT_RPC_HOST.value)
 
+    # Run checks
     if _SECURITY.value in 'positive_cases':
-        # Positive cases: mTLS, TLS, Plaintext
-        test_client.wait_for_active_server_channel()
-        client_sock: _Socket = test_client.get_active_server_channel_socket()
-        server_sock: _Socket = test_server.get_server_socket_matching_client(
-            client_sock)
-
-        server_tls = server_sock.security.tls
-        client_tls = client_sock.security.tls
-
-        print(f'\nServer certs:\n{debug_sock_tls(server_tls)}')
-        print(f'\nClient certs:\n{debug_sock_tls(client_tls)}')
-        print()
-
-        if server_tls.local_certificate:
-            eq = server_tls.local_certificate == client_tls.remote_certificate
-            print(f'(TLS)  Server local matches client remote: {eq}')
-        else:
-            print('(TLS)  Not detected')
-
-        if server_tls.remote_certificate:
-            eq = server_tls.remote_certificate == client_tls.local_certificate
-            print(f'(mTLS) Server remote matches client local: {eq}')
-        else:
-            print('(mTLS) Not detected')
-
+        positive_case_all(test_client, test_server)
     elif _SECURITY.value == 'mtls_error':
-        # Negative case
-        # Channel side
-        client_correct_setup = True
-        channel: _Channel = test_client.wait_for_server_channel_state(
-            state=_ClientChannelState.TRANSIENT_FAILURE)
-        try:
-            subchannel, *subchannels = list(
-                test_client.channelz.list_channel_subchannels(channel))
-        except ValueError:
-            print("(mTLS-error) Client setup fail: subchannel not found. "
-                  "Common causes: test client didn't connect to TD; "
-                  "test client exhausted retries, and closed all subchannels.")
-            return
-
-        logger.debug('Found subchannel, %r', subchannel)
-        if subchannels:
-            client_correct_setup = False
-            print(f'(mTLS-error) Unexpected subchannels {subchannels}')
-
-        subchannel_state: _ClientChannelState = subchannel.data.state.state
-        if subchannel_state is not _ClientChannelState.TRANSIENT_FAILURE:
-            client_correct_setup = False
-            print('(mTLS-error) Subchannel expected to be in '
-                  'TRANSIENT_FAILURE, same as its channel')
-
-        sockets = list(
-            test_client.channelz.list_subchannels_sockets(subchannel))
-        if sockets:
-            client_correct_setup = False
-            print(f'(mTLS-error) Unexpected subchannel sockets {sockets}')
-
-        if client_correct_setup:
-            print(f'(mTLS-error) Client setup pass: the channel '
-                  f'to the server has exactly one subchannel '
-                  f'in TRANSIENT_FAILURE, and no sockets')
-
-        # Server side
-        server_sockets = list(test_server.get_test_server_sockets())
-        if server_sockets:
-            print('(mTLS-error) Server setup fail:'
-                  f' unexpected sockets {sockets}')
-        else:
-            print(f'(mTLS-error) Server setup pass: test server has no sockets')
+        negative_case_mtls(test_client, test_server)
 
     test_client.close()
     test_server.close()
