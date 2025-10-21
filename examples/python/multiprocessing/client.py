b429eabb0d0c9b21abc0157e463bec46f5668b91
Sergii Tkachenko <sergiitk@google.com>
2025-10-20 19:21:25 -0700

    more experiments

--- examples/python/multiprocessing/client.py
+++ examples/python/multiprocessing/client.py
@@ -25,6 +25,8 @@ import operator
 import sys
 
 import grpc
+from grpc import _common
+
 import prime_pb2
 import prime_pb2_grpc
 
@@ -34,10 +36,17 @@ _MAXIMUM_CANDIDATE = 30
 # Each worker process initializes a single channel after forking.
 # It's regrettable, but to ensure that each subprocess only has to instantiate
 # a single channel to be reused across all RPCs, we use globals.
-_worker_channel_singleton = None
+# _worker_channel_singleton = None
 _worker_stub_singleton = None
 
 _LOGGER = logging.getLogger(__name__)
+
+
+
+def print_state(chan, connect=False):
+    result = chan._channel.check_connectivity_state(connect)
+    state = _common.CYGRPC_CONNECTIVITY_STATE_TO_CHANNEL_CONNECTIVITY[result]
+    print(f"Channel state: {state}")
 
 
 # def _shutdown_worker():
@@ -47,9 +56,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def _initialize_worker(_worker_channel):
-    global _worker_channel_singleton  # pylint: disable=global-statement
+    # global _worker_channel_singleton  # pylint: disable=global-statement
     global _worker_stub_singleton  # pylint: disable=global-statement
     _LOGGER.info("Initializing worker process.")
+    print_state(_worker_channel)
+
     _worker_stub_singleton = prime_pb2_grpc.PrimeCheckerStub(
         _worker_channel
     )
@@ -88,8 +99,16 @@ def main():
     )
     args = parser.parse_args()
 
-    with grpc.insecure_channel(args.server_address) as _worker_channel:
-        primes = _calculate_primes(_worker_channel)
+    chan = grpc.insecure_channel(args.server_address)
+    print_state(chan)
+
+    tmp_stub = prime_pb2_grpc.PrimeCheckerStub(chan)
+    tmp_stub.check(prime_pb2.PrimeCandidate(candidate=1))
+
+    print_state(chan)
+
+    primes = _calculate_primes(chan)
+
     print([prime for prime, result in primes if result])
     print('---- PASS ----')
 
@@ -99,5 +118,5 @@ if __name__ == "__main__":
     formatter = logging.Formatter("[PID %(process)d] %(message)s")
     handler.setFormatter(formatter)
     _LOGGER.addHandler(handler)
-    _LOGGER.setLevel(logging.INFO)
+    _LOGGER.setLevel(logging.DEBUG)
     main()
